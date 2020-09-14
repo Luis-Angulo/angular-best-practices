36f47cfdd1525d93dc1f9331ddea190075c92842
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-09-14 05:51:50 -0700

    lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
 import { appRoutes } from "./routes";
 import { AppComponent } from "./app.component";
-import { CatalogComponent } from "./catalog/catalog.component";
-import { RegisterComponent } from "./users/register.component";
-import { SignInComponent } from "./users/sign-in.component";
 import { CatalogRepositoryService } from "./catalog/catalog-repository.service";
 import { CoreModule } from "./core/core.module";
 import { SharedModule } from "./shared/shared.module";
 import { CatalogModule } from "./catalog/catalog.module";
+import { UsersModule } from "./users/users.module";
 
 @NgModule({
   imports: [
@@ -22,10 +20,11 @@ import { CatalogModule } from "./catalog/catalog.module";
     SharedModule,
     HttpModule,
     FormsModule,
+    UsersModule,
     ReactiveFormsModule,
     RouterModule.forRoot(appRoutes),
   ],
-  declarations: [AppComponent, RegisterComponent, SignInComponent],
+  declarations: [AppComponent],
   providers: [CatalogRepositoryService],
   bootstrap: [AppComponent],
 })
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { CatalogComponent } from './catalog/catalog.component';
-import { RegisterComponent } from './users/register.component';
-import { SignInComponent } from './users/sign-in.component';
 
+// Lazy loading is done using the router. Anything lazy-loaded must be its own module.
+// modules are loaded dynamically by the router when the route segment is activated
 export const appRoutes: Routes = [
   { path: 'catalog', component: CatalogComponent },
-  { path: 'users/register', component: RegisterComponent },
-  { path: 'users/sign-in', component: SignInComponent },
+  // on angular 7 and above, the syntax for loadChildren is different
+  { path: 'users', loadChildren: 'app/users/users.module#UsersModule' },
 ];
